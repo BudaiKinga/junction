2e22d6776dc79aa507685ffe57aa51caa99ccfca
BudaiKinga <budai_kinga@yahoo.com>
2019-10-26 12:36:55 +0200

    refactor requests

---
+++ src/main/java/request/RawRequest.java
@@ -0,0 +1,30 @@
+package request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.http.client.methods.HttpPost;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class StationRequest {
+
+    private static final String BASIC_URL = "https://api.hypr.cl/station";
+
+    private static final String API_KEY = "iQ0WKQlv3a7VqVSKG6BlE9IQ88bUYQws6UZLRs1B";
+    private String command;
+    private Date timeStart;
+    private Date timeStop;
+
+    public HttpPost postRequest() {
+        HttpPost httpPost = new HttpPost(BASIC_URL);
+        httpPost.addHeader("x-api-key", API_KEY);
+        httpPost.addHeader("command", command);
+        httpPost.addHeader("timeStart", RequestDateFormatter.getFormattedDate(timeStart));
+        httpPost.addHeader("timeStop", RequestDateFormatter.getFormattedDate(timeStop));
+
+        return httpPost;
+    }
+
+}
